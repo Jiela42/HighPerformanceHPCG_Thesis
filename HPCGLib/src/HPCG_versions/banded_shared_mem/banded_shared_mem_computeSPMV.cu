6a17f35509638cb84f5e4daa8d45de131224e625
Daniela Knecht <dknecht@ault.cscs.ch>
2024-10-30 17:30:18 +0100

    moving precomputation to the not kernel call

--- HPCGLib/src/HPCG_versions/banded_shared_mem/banded_shared_mem_computeSPMV.cu
+++ HPCGLib/src/HPCG_versions/banded_shared_mem/banded_shared_mem_computeSPMV.cu
@@ -2,6 +2,22 @@
 #include "UtilLib/utils.cuh"
 #include <cuda_runtime.h>
 
+void eliminate_overlap(int* min_j, int* max_j, int num_thick_bands){
+
+    int num_consecutive_memory_regions = 1;
+
+    for (int i = 1; i < num_thick_bands; i++){
+        if (max_j[num_consecutive_memory_regions] <= min_j[i]){
+            max_j[num_consecutive_memory_regions] = max_j[i];
+        }
+        else{
+            min_j[num_consecutive_memory_regions] = min_j[i];
+            max_j[num_consecutive_memory_regions] = max_j[i];
+            num_consecutive_memory_regions ++;
+        }
+    }
+}
+
 template <typename T>
 void Banded_Shared_Memory_Implementation<T>::banded_shared_memory_computeSPMV(
         banded_Matrix<T>& A, //we only pass A for the metadata
@@ -27,15 +43,56 @@ void Banded_Shared_Memory_Implementation<T>::banded_shared_memory_computeSPMV(
             if (not j_min_i_host[i-1] + 1 == j_min_i_host[i]){
                 new_elem_per_row ++;
             }
-        }
+        }    
 
         int shared_mem_bytes = 1024 * SHARED_MEM_SIZE;
         int shared_mem_doubles = shared_mem_bytes / sizeof(double);
 
         int rows_per_sm = (shared_Mem_doubles -  2 * num_bands) / new_elem_per_row
 
+        // based on the guess of the number of rows per sm we calculate the exact numbers
+        
+        int [num_bands] min_j;
+        int [num_bands] max_j;
+        int [num_bands] consecutive_j;
+        
+        // in the kernel we will need to adjust this, by adding the row start
+        min_j[0] = j_min_i[0];
+        max_j[0] = j_min_i[0] + rows_per_sm;
+        consecutive_j[0] = rows_per_sm;
 
-        // shared_j_min_i = num_bands * size_of(int);
+        // this refers to how many independent bands we end up having
+        int num_thick_bands = 1;
+
+        for (int band = 1; band < num_bands; band++){
+            
+            if (j_min_i[band-1] + 1 == j_min_i[band]){
+                max_j[num_thick_bands-1] += 1;
+                consecutive_j[num_thick_bands-1] += 1
+            }
+            else{
+                // again in the kernel this will need to be adjusted
+                min_j[num_thick_bands] = j_min_i[band];
+                max_j[num_thick_bands] = j_min_i[band] + num_rows_per_sm;
+                consecutive_j[num_thick_bands] = num_rows_per_sm;
+                num_thick_bands ++;
+            }
+        }
+        
+        // you need to re-work this!
+        // in the eliminate overlap!!!
+        //just hand in a pointer to an int.
+        int num_x_elem = 0;
+        for(int i = 0; i < num_thick_bands; i++){
+            num_x_elem += consecutive_j[i];
+        }
+
+        eliminate_overlap(min_j, max_j, num_thick_bands);
+
+        int size_shared_j_min_i = num_bands * size_of(int);
+        int size_shared_x = num_x_elem * size_of(double);
+        int size_x_offsets = 
+        int size_shared_memory = size_shared_j_min_i + size_shared_x;
         // shared_x = (rows_per_sm -1) * new_elem_per_row + num_bands;
 
         // call the kernel
--- HPCGLib/src/HPCG_versions/banded_shared_mem/banded_shared_mem_kernel_SPMV.cu
+++ HPCGLib/src/HPCG_versions/banded_shared_mem/banded_shared_mem_kernel_SPMV.cu
@@ -17,40 +17,9 @@
             // every thread computes one or more rows of the matrix
             for (int iter = 0; iter*total_size < num_rows; iter++) {
 
-                // between each row of the matrix, we need to allocate the new entries of x
-                int [num_bands] min_j;
-                int [num_bands] max_j;
-                int [num_bands] consecutive_j;
                 // row_start refers to the first row of this SM
                 int row_start = iter * total_size + bid;
 
-                min_j[0] = j_min_i[0] - row_start;
-                max_j[0] = j_min_i[0] - (row_start + num_rows_per_sm);
-                consecutive_j[0] = num_rows_per_sm;
-
-
-                // this refers to how many independent bands we end up having
-                int num_thick_bands = 1;
-                
-
-                for (int band = 1; band < num_bands; band++){
-                    
-                    if (j_min_i[band-1] + 1 == j_min_i[band]){
-                        max_j[num_thick_bands-1] += 1;
-                        consecutive_j[num_thick_bands-1] += 1
-                    }
-                    else{
-                        min_j[num_thick_bands] = j_min_i[band] - row_start;
-                        max_j[num_thick_bands] = j_min_i[band] - (row_start + num_rows_per_sm);
-                        consecutive_j[num_thick_bands] = num_rows_per_sm;
-                        num_thick_bands ++;
-                    }
-                }
-                
-                int num_x_elem = 0;
-                for(int i = 0; i < num_thick_bands; i++){
-                    num_x_elem += consecutive_j[i];
-                }
 
                 // now we actually load x
 
