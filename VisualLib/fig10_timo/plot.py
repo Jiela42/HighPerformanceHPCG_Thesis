b2ec2956249b02e8dce7103a551a595a89c0a88d
Timo Schneider <timos@inf.ethz.ch>
2025-04-11 11:41:16 +0200

    suggestion for fig 10 (data is fake, only for plot prototyping)

---
+++ VisualLib/fig10_timo/plot.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the data
+data = pd.read_csv('data.dat', sep=r'\s+')
+# Normalize by time/nnz * gpus
+data['Runtime'] = (data['Runtime'] / data['NNZ']) * data['GPUs']
+
+# Create the line plot
+sns.set(style="whitegrid")
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=data, x='GPUs', y='Runtime', marker='o', errorbar=('ci', 95)) #make sure we use the same errorbar setting everywhere! 
+
+# Axis labels
+plt.xlabel('Number of GPUs')
+plt.ylabel('Normalized Runtime [s]')
+
+# Show the plot
+plt.tight_layout()
+#plt.show()
+plt.savefig("fig10.pdf")
